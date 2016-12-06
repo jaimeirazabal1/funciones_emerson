a27f34cf8fc8010101d3d76588dfcd00e00f485b
Jaime Irazabal <jaimeirazabal1@gmail.com>
2016-12-06 15:19:04 -0430

    terminando la plantilla

--- functions.php
+++ functions.php
@@ -1,5 +1,7 @@
 <?php
-
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 // # Importa los datos de un archivo de excel o csv a una tabla
 // importData(String tabName, String filePath) {
 //     ...
@@ -51,6 +53,7 @@ function getColumns($tabName){
 // # Lista y filtra una tabla
 function getList($tabName, $filterName, $filterValue) {
     global $mysqli;
+
     $header=getColumns($tabName);
     
     $sql="SELECT * FROM $tabName WHERE $filterName='$filterValue'";
@@ -115,8 +118,220 @@ function rmRow($tabName, $id) {
 	}
     return true;
 }
-// var_dump(editRow('usuario','2',array('username'=>'Jaime 3','nombre'=>'Jaime Irazabal 2','cedula'=>'16923509 2')));
-rmRow('usuario',2);
-echo getList('usuario','username','tayme');
+//hacer un csv de una tabla
+/*
+	Primer parametro el nombre de la tabla en la base de datos
+	Segundo parametro el nombre del archivo csv, es opcional
+*/
+function exportData($tabName,$csvName=NULL){
+	global $mysqli;
 
-?>
+	if (!$csvName) {
+		$csvName = $tabName;
+	}
+	$list[] = getColumns($tabName);
+	$list_ = $list;
+
+	$sql = "SELECT * from $tabName";
+
+	if (!$resultado = $mysqli->query($sql)) {
+		showErrors();
+	}
+	for ($i=0; $i < count($list_) ; $i++) { 
+		foreach ($resultado->fetch_all(MYSQLI_NUM) as $key => $value) {
+			$list[]=$value;
+			
+		}
+	}
+	$file = fopen($csvName.".csv","w");
+
+	foreach ($list as $line)
+	{
+		fputcsv($file,$line,";");
+	}
+
+	fclose($file);
+}
+/*
+	esta funcion toma los datos de un csv que se pasa en el segundo parametro
+	genera una tabla con el nombre que se le pasa en el primer parametro
+	$tabName -> nombre de la tabla a crear.
+	$filePath -> archivo .csv de donde se tomaran los datos.
+*/
+function importData($tabName, $filePath) 
+{
+	global $mysqli;
+	if (!file_exists($filePath)) {
+		die("Error: El archivo ".$filePath." No existe!");
+	}
+	$data = array();
+	if (($gestor = fopen($filePath, "r")) !== FALSE) {
+		while ($datos = fgetcsv($gestor, 1000, ";")) {
+			$data[]=$datos;
+		}
+	    
+	    fclose($gestor);
+	}	
+	$create="CREATE TABLE IF NOT EXISTS `$tabName` (
+	  `id` int(8) NOT NULL AUTO_INCREMENT,
+	  `name` varchar(100) NOT NULL,
+	  `author` varchar(30) NOT NULL,
+	  `isbn` varchar(30) NOT NULL,
+	  PRIMARY KEY (`id`)
+	) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1";
+
+	$insert = "INSERT INTO $tabName (";
+
+	$create="CREATE TABLE IF NOT EXISTS `$tabName` (";
+	for ($i=0; $i < count($data[0]) ; $i++) { 
+		if ($i==count($data[0])-1) {
+			$insert.=strtolower($data[0][$i]." )");
+			$create.=" `".$data[0][$i]."` varchar(200)";
+		}else{
+			$insert.=strtolower($data[0][$i].",");
+
+			$create.=" `".$data[0][$i]."` varchar(200),";
+		}
+	}
+	$create.=") ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1;";
+
+	$insert.=" VALUES ";
+	for ($j=1; $j < count($data); $j++) { 
+		if ($j != 1) {
+			# code...
+			$insert.=", ( ";
+
+		}else{
+
+			$insert.=" ( ";
+		}
+		for ($i=0; $i < count($data[$j]) ; $i++) { 
+			if ($i==count($data[$j])-1) {
+				$insert.="
+				'".strtolower($data[$j][$i]."' )");
+				//$create.=" `".$data[$j][$i]."` varchar(200)";
+			}else{
+				$insert.="
+				'".strtolower($data[$j][$i]."',");
+
+				//$create.=" `".$data[$j][$i]."` varchar(200),";
+			}
+		}
+	}
+	
+	if (!$mysqli->query($create)) {
+		showErrors();
+	}
+
+
+	
+	if (!($mysqli->query($insert))) {
+	    echo "\nQuery execute failed: ERRNO: (" . $mysqli->errno . ") " . $mysqli->error;
+	    die;
+	};
+
+	return true;
+}
+
+# Genera y guarda una plantilla de reporte
+/*function saveReportTemplate(File logo1, File logo2, String title,
+	String subtitle, String frt, String notes,
+	String firmText1, String firmText2, String botInfo,
+	String footer) {
+	...
+}
+*/
+# Genera y guarda una plantilla de reporte
+function saveReportTemplate($logo1=null,$logo2=null, $title=null,
+	$subtitle=null, $frt=null, $notes=null,
+	$firmText1=null, $firmText2=null,$botInfo=null,
+	$footer=null,$content=null) {
+
+		?>
+	<style type="text/css">
+		body{
+			margin:auto 5% ;
+		}
+	</style>
+	<table class="table" cellspacing="0" width="100%">
+		<thead>
+			<td width="50%">
+				<?php if ($logo1 and file_exists($logo1)): ?>
+					<img width="400" height="200" src="<?php echo $logo1 ?>" alt="">
+				<?php else: ?>
+					<img src="" alt="IMAGEN NO ENCONTRADA">
+				<?php endif ?>
+			</td>
+			<td width="50%" style="text-align: right;">
+				<?php if ($logo2 and file_exists($logo2)): ?>
+					<img width="300" height="100" src="<?php echo $logo2 ?>" alt="">
+				<?php else: ?>
+					<img src="" alt="IMAGEN NO ENCONTRADA">
+				<?php endif ?>				
+			</td>
+		</thead>
+		
+		
+	</table>
+	<table class="table" cellspacing="0" width="100%">
+		<tr>
+			<td style="width: 33%"></td>
+			<td style="width: 33%;text-align: center"><?php echo strtoupper($title) ?></td>
+			<td style="width: 33%"></td>
+		</tr>
+	</table>
+	<table class="table" cellspacing="0" width="100%">
+		<tr>
+			<td style="width: 50%"></td>
+			<td style="width: 50%;text-align: center"><?php echo strtoupper($subtitle) ?></td>
+		</tr>
+
+	</table>
+	<table class="table" cellspacing="0" width="100%">
+		<tr>
+			<td style="width: 25%"></td>
+			<td style="width: 25%"></td>
+			<td style="width: 25%"></td>
+			<td style="width: 25%;text-align: center;font-size: 10px"><?php echo strtoupper($frt) ?></td>
+		</tr>
+	</table>
+	<p>
+		<?php echo $content ?>
+	</p>
+	<table class="table" cellspacing="0" width="100%">
+		<tr>
+			<td style="width: 100%">
+				<p style="text-align: justify;padding: 10px">
+					<?php echo $notes ?>
+				</p>
+			</td>
+		</tr>
+	</table>
+	<table class="table" cellspacing="0" width="100%">
+		<tr>
+			<td style="width: 50%;text-align: center"><?php echo strtoupper($firmText1) ?></td>
+			<td style="width: 50%;text-align: center"><?php echo strtoupper($firmText2) ?></td>
+		</tr>
+
+	</table>
+	<table class="table" cellspacing="0" width="100%">
+		<tr>
+			<td style="width: 100%">
+				<p style="text-align: justify;padding: 10px">
+					<?php echo $botInfo ?>
+				</p>
+			</td>
+		</tr>
+	</table>
+	<table class="table" cellspacing="0" width="100%">
+		<tr>
+			<td style="width: 100%">
+				<p style="text-align: center;padding: 10px">
+					<?php echo $footer ?>
+				</p>
+			</td>
+		</tr>
+	</table>
+		<?php
+
+}
---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Funciones de Emerson</title>
+	<?php require_once("functions.php") ?>
+</head>
+<body>
+	<?php echo saveReportTemplate("logo1.png",
+									"logo2.png",
+									"TITULO",
+									"LABORATORIO DE SERVICIOS - ANALISIS DE AGUAS",
+									"AQUI OTRO TEXTO",
+									"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem ab amet, beatae ratione voluptatibus! Eius dolor a non pariatur aperiam commodi velit voluptatem. Voluptates, numquam eos tenetur consequuntur consequatur nobis.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem ab amet, beatae ratione voluptatibus! Eius dolor a non pariatur aperiam commodi velit voluptatem. Voluptates, numquam eos tenetur consequuntur consequatur nobis.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem ab amet, beatae ratione voluptatibus! Eius dolor a non pariatur aperiam commodi velit voluptatem. Voluptates, numquam eos tenetur consequuntur consequatur nobis.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem ab amet, beatae ratione voluptatibus! Eius dolor a non pariatur aperiam commodi velit voluptatem. Voluptates, numquam eos tenetur consequuntur consequatur nobis.",
+									"FIRMA1",
+									"FIRMA2","","FOOTER"); ?>
+</body>
+</html>
